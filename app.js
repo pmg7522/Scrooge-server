73cbdf3e41decd557d0b3bd0bdc650ee75d173f6
chanyang217 <chanyang721@gmail.com>
2021-06-23 17:42:16 +0900

    Check Deployment

--- app.js
+++ app.js
@@ -18,43 +18,47 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
 const port = 3000; // 배포환경 http: 80 // https: 443
 
-// users
-app.post("/signup", upload.single('photo'), controllers.signup);
-app.post("/fixuserinfo", upload.single('photo'), controllers.fixuserinfo);
-app.post("/login", controllers.login);
-app.post("/kakaologin", controllers.kakaologin);
-app.post("/googlelogin", controllers.googlelogin);
-app.get("/signout", controllers.signout);
-app.get("/deleteuser", controllers.deleteuser);
-app.get("/initialize", controllers.initialize);
+app.get("/", (req, res) => {
+    console.log("Hello World")
+})
 
-// settings
-app.post("/darkmode", controllers.darkmode);
-app.post("/changemainpage", controllers.changemainpage);
-app.get("/deletedata", controllers.deletedata);
+// // users
+// app.post("/signup", upload.single('photo'), controllers.signup);
+// app.post("/fixuserinfo", upload.single('photo'), controllers.fixuserinfo);
+// app.post("/login", controllers.login);
+// app.post("/kakaologin", controllers.kakaologin);
+// app.post("/googlelogin", controllers.googlelogin);
+// app.get("/signout", controllers.signout);
+// app.get("/deleteuser", controllers.deleteuser);
+// app.get("/initialize", controllers.initialize);
 
-// category
-app.post("/fixcategoryinfo", controllers.fixcategoryinfo);
-app.get("/budget", controllers.budget);
-app.get("/deletecategory", controllers.deletecategory);
-app.get("/getcategoryinfo", controllers.getcategoryinfo);
+// // settings
+// app.post("/darkmode", controllers.darkmode);
+// app.post("/changemainpage", controllers.changemainpage);
+// app.get("/deletedata", controllers.deletedata);
 
-// day
-app.post("/editspendmoney", controllers.editspendmoney);
-app.post("/createspendmoney", controllers.createspendmoney);
-app.post("/deletespendmoney", controllers.deletespendmoney);
-app.get("/daypage", controllers.daypage);
+// // category
+// app.post("/fixcategoryinfo", controllers.fixcategoryinfo);
+// app.get("/budget", controllers.budget);
+// app.get("/deletecategory", controllers.deletecategory);
+// app.get("/getcategoryinfo", controllers.getcategoryinfo);
 
-// month
-app.get("/getmonthlydata", controllers.getmonthlydata);
+// // day
+// app.post("/editspendmoney", controllers.editspendmoney);
+// app.post("/createspendmoney", controllers.createspendmoney);
+// app.post("/deletespendmoney", controllers.deletespendmoney);
+// app.get("/daypage", controllers.daypage);
 
-// year
-app.get("/getyearlydata", controllers.getyearlydata);
+// // month
+// app.get("/getmonthlydata", controllers.getmonthlydata);
+
+// // year
+// app.get("/getyearlydata", controllers.getyearlydata);
 
 let server;
-server = app.listen(port,() => {
+
+server = app.listen(port, () => {
     console.log(`서버가 ${port}번에서 작동중입니다.`)
 })
--- controllers/users/deleteuser.js
+++ controllers/users/deleteuser.js
@@ -1,8 +1,28 @@
 const { user, money, achievement, category, level } = require('../../models');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
 dotenv.config();
 
-module.exports = (req, res) => {
-    
+module.exports = async (req, res) => {
+    const authorization = req.headers['authorization'];
+
+    if (!authorization) { 
+        return res.status(401).send({ "message": 'invalid access token'})
+    }
+    const token = authorization.split(' ')[1];
+    const data = jwt.verify(token, process.env.ACCESS_SECRET);
+
+    const joinId = await tag_store.findAll({ where: { storeId: data.storeId }})
+    const joinInfo = joinId.map(el => el.dataValues.tagId)
+
+    for(let el of joinInfo){
+        await tag.destroy({ where: { id: el }})
+    }
+
+    await user.destroy({ where: { id: data.id }})
+    await store.destroy({ where: { id: data.storeId }})
+    await item.destroy({ where: { storeId: data.storeId }})
+    await tag_store.destroy({ where: { storeId: data.storeId }})
+
+    return res.status(205).send({ "message": '회원탈퇴 완료' })
 }

e797b35f0ead832d0ef9ace9fb3def6a381353e1
pmg7522 <pmg752290@gmail.com>
2021-06-25 13:50:46 +0900

    Feat FixCategory

--- controllers/category/fixcategoryinfo.js
+++ controllers/category/fixcategoryinfo.js
@@ -4,5 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = (req, res) => {
-    
+    const{ categoryId, categoryname, budget } = req.body;
+
+    const authorization = req.headers["authorization"];
+  
+    if (!authorization) { 
+      return res.status(401).send({ "message": "Unauthorized" })
+    }
+
+    await category.update({ categoryname, budget }, { where: { id: categoryId }})
+
+    return res.status(200).send({ "message": "카테고리 정보 수정 완료" })
 }

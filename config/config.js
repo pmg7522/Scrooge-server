d4e3f459a8ad3c4b322b2e87caa7294e3b6805b2
pmg7522 <pmg752290@gmail.com>
2021-06-25 11:41:28 +0900

    Feat CreateSpendMoney

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": "1q2w",
+    "password": "alsrb12",
     "database": "scrooge",
     "host": "127.0.0.1",
     "dialect": "mysql"
---
+++ controllers/tokenFunctions/index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const { sign, verify } = require("jsonwebtoken");
+
+module.exports = {
+  generateAccessToken: (data) => {
+    return sign(data, process.env.ACCESS_SECRET, { expiresIn: "1h" });
+  },
+  generateRefreshToken: (data) => {
+    return sign(data, process.env.REFRESH_SECRET, { expiresIn: "2h" });
+  },
+  sendRefreshToken: (res, refreshToken) => {
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+    });
+  },
+  sendAccessToken: (res, accessToken) => {
+    return res.json({ data: { accessToken }, message: "ok" });
+  },
+  resendAccessToken: (res, accessToken, data) => {
+    return res.json({ data: { accessToken, user: data }, message: "Token 재발급 완료" });
+  },
+  isAuthorized: (req) => {
+    const authorization = req.headers["authorization"];
+    if (!authorization) {
+      return res.status(401).send({ "message": 'invalid access token'})
+    }
+    const token = authorization.split(' ')[1];
+    const data = jwt.verify(token, process.env.ACCESS_SECRET); // 해당 유저의 user 정보
+    if (!data) {
+        return res.status(404).send({ data: null, message: "토큰이 없는 잘못된 접근입니다." })
+    }
+  },
+  checkRefeshToken: (refreshToken) => {
+    try {
+      return verify(refreshToken, process.env.REFRESH_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+};

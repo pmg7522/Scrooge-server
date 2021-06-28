256e0082f18e8df215f370a482926137bd2e08a5
chanyang217 <chanyang721@gmail.com>
2021-06-28 11:58:55 +0900

    Refactor users

--- controllers/functions/index.js
+++ controllers/functions/index.js
@@ -10,34 +10,18 @@ module.exports = {
   generateRefreshToken: (data) => {
     return sign(data, process.env.REFRESH_SECRET, { expiresIn: "2h" });
   },
-<<<<<<< HEAD:controllers/functions/index.js
-
-  sendToken: (res, accessToken, refreshToken) => {
-    return res.
-    cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-    })
-    .json({ data: { accessToken, refreshToken }, message: "ok" });
-=======
   sendToken: (res, accessToken, refreshToken) => {
     return res.cookie("refreshToken", refreshToken, {
       sameSite: "none",
       secure: true,
       httpOnly: true
     }).send({ data: { accessToken, refreshToken }, message: "로그인 완료" });
->>>>>>> caf65b5c16db07098af2690854d3bc776cf82f93:controllers/tokenFunctions/index.js
   },
 
   resendAccessToken: (res, accessToken, data) => {
     return res.send({ data: { accessToken, user: data }, message: "Token 재발급 완료" });
   },
-<<<<<<< HEAD:controllers/functions/index.js
-  
-
-  isAuthorized: (req) => {
-=======
   isAuthorized: (req, res) => {
->>>>>>> caf65b5c16db07098af2690854d3bc776cf82f93:controllers/tokenFunctions/index.js
     const authorization = req.headers["authorization"];
     if (!authorization) {
       return res.status(401).send({ "message": 'invalid access token'})
@@ -49,13 +33,7 @@ module.exports = {
     }
     return data;
   },
-<<<<<<< HEAD:controllers/functions/index.js
-
-
-  checkRefeshToken: (refreshToken) => {
-=======
   checkRefreshToken: (refreshToken) => {
->>>>>>> caf65b5c16db07098af2690854d3bc776cf82f93:controllers/tokenFunctions/index.js
     try {
       return verify(refreshToken, process.env.REFRESH_SECRET);
     } catch (err) {

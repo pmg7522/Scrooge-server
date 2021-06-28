304da00d3ee1896a50abcdcdeed00e914ea939d7
pmg7522 <pmg752290@gmail.com>
2021-06-28 13:43:50 +0900

    Refactor Day Page

--- controllers/day/createspendmoney.js
+++ controllers/day/createspendmoney.js
@@ -1,25 +1,24 @@
 const { money, category } = require('../../models');
-const jwt = require("jsonwebtoken");
+const{ isAuthorized } = require("../functions");
 const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = async (req, res) => {
-    const authorization = req.headers["authorization"];
-  
-    if (!authorization) { 
-      return res.status(401).send({ "message": "Unauthorized" })
-    }
-    
-    const token = authorization.split(" ")[1];
-    const data = jwt.verify(token, process.env.ACCESS_SECRET);
+  const data = isAuthorized(req);
 
+  if(data){
     const { cost, memo, date, categoryname } = req.body
-
+  
     const categoryInfo = await category.findOne({ where: { categoryname }, raw: true})
     const categoryId = categoryInfo.id
-
+  
     await money.findOne({ where: { userId: data.id } })
-    await money.create({ cost, memo, date, userId: data.id ,categoryId })
-
+    await money.create({ cost, memo, date, userId: data.id, categoryId })
+  
     return res.status(201).send({ "message": "생성 완료" })
+  }
+  else{
+    console.log(err)
+    return res.status(500).send({ message: "Internal Server Error" })
+  }
 }
--- controllers/day/deletespendmoney.js
+++ controllers/day/deletespendmoney.js
@@ -1,17 +1,20 @@
 const { money } = require('../../models');
-const jwt = require("jsonwebtoken");
+const{ isAuthorized } = require("../functions");
 const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = async (req, res) => {
+  const data = isAuthorized(req);
+  
+  if(data){
     const { moneyId } = req.body
-    const authorization = req.headers["authorization"];
-    
-    if (!authorization) { 
-      return res.status(401).send({ "message": "Unauthorized" })
-    }
-
+  
     await money.destroy({ where: { id: moneyId } })
-
+  
     res.status(200).send({ message: "삭제 완료" })
+  }
+  else{
+    console.log(err)
+    return res.status(500).send({ message: "Internal Server Error" })
+  }
 }

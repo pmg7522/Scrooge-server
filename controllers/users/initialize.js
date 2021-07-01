f22c25f01242cc6bbbae9983d119830ced314d6b
chanyang217 <chanyang721@gmail.com>
2021-07-01 15:18:13 +0900

    Fix Deployment Bug

--- controllers/users/initialize.js
+++ controllers/users/initialize.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const imageDataUri = require("image-data-uri");
 
 module.exports = async (req, res) => {
-  try{
+//  try{
     const data = isAuthorized(req);
     
     if(data){
@@ -13,7 +13,7 @@ module.exports = async (req, res) => {
         const { username, email, experience, darkmode, redirect } = userData;
         let result = fs.readFileSync(`.${data.photo}`);
         let realphoto = imageDataUri.encode(result, "jpg");
-      
+        
         return res.status(200).send({ data: 
           { userInfo: { username, email, photo: realphoto, level: 1, experience }, userset: { darkmode, redirect }}});
       })
@@ -25,8 +25,8 @@ module.exports = async (req, res) => {
     else{
       return res.status(409).send({ message: "Invalid Data" });
     }
-  }
-  catch(err){
-    console.log(err)
-  }
+  // }
+  // catch(err){
+  //   console.log(err)
+  // }
 }

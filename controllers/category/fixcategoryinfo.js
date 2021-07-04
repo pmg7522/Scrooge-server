27754745da5bc350344c0d1349e1bf16f2b6ed5d
chanyang217 <chanyang721@gmail.com>
2021-07-04 10:22:10 +0900

    Fix Bug Fixcategoryinfo

--- controllers/category/fixcategoryinfo.js
+++ controllers/category/fixcategoryinfo.js
@@ -3,15 +3,23 @@ const { isAuthorized } = require("../functions");
 
 module.exports = async (req, res) => {
   const data = isAuthorized(req);
-  
+  const{ categoryId, categoryname, budget } = req.body;
+
   try{
     if(data){
-      const{ categoryId, categoryname, budget } = req.body;
-      const newBudget = budget.split(",").join("")
+      if (!budget) {
+        
+        await category.update({ categoryname }, { where: { id: categoryId } });
+
+        return res.status(200).send({ "message": "카테고리 정보 수정 완료" });
+      } 
+      else {
+        const newBudget = budget.split(",").join("")
   
-      await category.update({ categoryname, budget: newBudget }, { where: { id: categoryId } });
-  
-      return res.status(200).send({ "message": "카테고리 정보 수정 완료" });
+        await category.update({ categoryname, budget: newBudget }, { where: { id: categoryId } });
+    
+        return res.status(200).send({ "message": "카테고리 정보 수정 완료" });
+      }
     }
     else{
       console.log(err);

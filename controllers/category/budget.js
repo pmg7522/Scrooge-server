6ecfbdda1866baf04b61cc1fc8ed508536ac648a
chanyang217 <chanyang721@gmail.com>
2021-07-05 17:57:25 +0900

    Fix achievement Table and Floor scrooge

--- controllers/category/budget.js
+++ controllers/category/budget.js
@@ -5,21 +5,21 @@ const sequelize = require("sequelize");
 module.exports = async (req, res) => {
     try{
         const data = isAuthorized(req);
-    
         if(data){
             const categoryMoney = await money.findAll({ 
                 attributes: [[ sequelize.fn("sum", sequelize.col("cost")), "allCost" ]],
-                include: [{model: category, attributes: ["id","categoryname","budget"]}],
+                include: [{model: category, attributes: ["id","categoryname","budget", "emoji"]}],
                 group: "category.id" ,
                 where: { userId: data.id }})
-                
+
                 let categories = [];
                 for(let i = 0; i < categoryMoney.length; i++){
                     let categoryname = categoryMoney[i].dataValues.category.categoryname;
                     let categorybudget = categoryMoney[i].dataValues.category.budget;
                     let categoryused = Number(categoryMoney[i].dataValues.allCost);
+                    let categoryemoji = categoryMoney[i].dataValues.category.emoji;
                     let categoryrest = categoryMoney[i].dataValues.category.budget - categoryMoney[i].dataValues.allCost;
-                    let allData = { categoryname, categorybudget, categoryused ,categoryrest };
+                    let allData = { categoryname, categorybudget, categoryused ,categoryrest, categoryemoji };
                     categories.push(allData);
                 }
         

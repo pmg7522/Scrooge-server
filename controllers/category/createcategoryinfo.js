ac51954a7f6f2890b835130d269f3a5bf64a3a48
chanyang217 <chanyang721@gmail.com>
2021-07-05 09:31:10 +0900

    Refactor createcategoryinfo duplicated name

--- controllers/category/createcategoryinfo.js
+++ controllers/category/createcategoryinfo.js
@@ -5,11 +5,27 @@ module.exports = async (req, res) => {
   try{
     const data = isAuthorized(req);
     const{ categoryname, budget, emoji } = req.body;
+    const categoryinfo = await category.findOne({ where: { userId: data.id, categoryname } })
+    console.log(categoryname)
+    if (categoryinfo) {
+      return res.status(409).send({ message: "중복된 카테고리가 존재합니다" })
+    }
 
     if(data){
+      if(!budget) {
+        if (!emoji) {
+          await category.create({ categoryname, userId: data.id });
+          return res.status(201).send({ "message": "생성 완료" });
+        } 
+        else {
+          await category.create({ categoryname, emoji, userId: data.id });
+          return res.status(201).send({ "message": "생성 완료" });
+        }
+      }
+      else {
         if (!emoji) {
           const newBudget = budget.split(",").join("")
-          await category.create({ categoryname, budget: newBudget ,userId: data.id });
+          await category.create({ categoryname, budget: newBudget, userId: data.id });
           return res.status(201).send({ "message": "생성 완료" });
         } 
         else {
@@ -17,6 +33,7 @@ module.exports = async (req, res) => {
           await category.create({ categoryname, budget: newBudget, emoji, userId: data.id });
           return res.status(201).send({ "message": "생성 완료" });
         }
+      }
     }
     else{
       console.log(err);
